0e177f90cd74845a87f6f2a1fc75fc715439b054
rouge <rouge_3877@outlook.com>
2024-07-12 15:50:53 +0800

    init

---
+++ init.sh
@@ -0,0 +1,20 @@
+sudo apt install gcc gdb tmux zsh neofetch
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+
+
+
+
+
+list=(zsh git tmux nvim )
+for i in ${list[*]}; do
+    stow -t $HOME $i || exit -1
+done
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+
