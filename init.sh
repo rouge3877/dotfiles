4d86f07bf415893303058d22ab4f180fca81f908
rouge <rouge_3877@outlook.com>
2024-07-12 16:09:08 +0800

    remove .github

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
+chsh -s $(which zsh)
 
-
-
+ZSH="$HOME/.dotfiles/oh-my-zsh" sh install.sh
 
 
 list=(zsh git tmux nvim )
@@ -17,4 +17,5 @@ done
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
-
+cd ~/dotfiles/tmux/.tmux
+git clone https://github.com/gpakosz/.tmux.git
