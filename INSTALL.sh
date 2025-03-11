eaa3b5b17f4b2112fa2bb41b7d38c9a65c7c24aa
Yuxuan Li <rouge_3877@outlook.com>
2025-03-11 13:17:17 +0800

    🔨 Update install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -10,16 +10,20 @@
 echo "Preparing to install dotfiles..."
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_SCRIPTS="$DOTFILES/SCRIPTS"
+DOTFILES_BACKUP="$DOTFILES/.backup"
 echo "Dotfiles directory: $DOTFILES"
 
 
 # Install the dependencies
 echo "Installing dependencies..."
-source $DOTFILES_SCRIPTS/dependencies.sh
+# source $DOTFILES_SCRIPTS/dependencies.sh
 
 # Symlink the dotfiles
 echo "Symlinking dotfiles..."
-source $DOTFILES_SCRIPTS/symlink.sh
+source $DOTFILES_SCRIPTS/install-vim.sh
+vim_install
+source $DOTFILES_SCRIPTS/install-tmux.sh
+tmux_install
 
 echo "Done!"
 echo "Please restart your terminal to see the changes"
