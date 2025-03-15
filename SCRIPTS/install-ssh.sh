4d7bb71d5ecacd98eecc10aa1f29b067950fbef0
Yuxuan Li <rouge_3877@outlook.com>
2025-03-15 14:09:23 +0800

    ✨ Update ssh install

--- SCRIPTS/install-ssh.sh
+++ SCRIPTS/install-ssh.sh
@@ -7,11 +7,17 @@ SSH_BACKUP_DIR=$DOTFILES_BACKUP/ssh
 
 
 _ssh_backup_and_make_symlinks() {
+
+    # Check if ssh directory exists
+    if [ ! -d "~/.ssh" ]; then
+        mkdir -p ~/.ssh
+    fi
+
     echo "Backing up existing ssh configuration..."
-    _backup_existing "$HOME/.ssh" "$SSH_BACKUP_DIR"
+    _backup_existing "$HOME/.ssh/config" "$SSH_BACKUP_DIR/config"
 
     # create symlinks
-    _create_symlink "$DOTFILES/ssh" "$HOME/.ssh"
+    _create_symlink "$DOTFILES/ssh/config" "$HOME/.ssh/config"
 }
 
 ssh_install() {
