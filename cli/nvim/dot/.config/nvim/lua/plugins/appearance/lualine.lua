268f300926d62b823c93efd237e8242c903d8402
Yuxuan Li <rouge_3877@outlook.com>
2025-09-21 13:35:45 +0800

    ✨ Update lualine configuration for improved appearance and functionality

--- cli/nvim/dot/.config/nvim/lua/plugins/appearance/lualine.lua
+++ cli/nvim/dot/.config/nvim/lua/plugins/appearance/lualine.lua
@@ -7,16 +7,16 @@ return {
             options = {
                 icons_enabled = true,
                 theme = 'moonfly',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' },
                 disabled_filetypes = {
                     statusline = {},
-                    winbar = {},
+                    winbar = {'NvimTree', 'neo-tree'},
                 },
-                ignore_focus = {},
+                ignore_focus = {'NvimTree', 'neo-tree'},
                 always_divide_middle = true,
-                always_show_tabline = true,
-                globalstatus = false,
+                always_show_tabline = false,
+                globalstatus = true,
                 refresh = {
                     statusline = 1000,
                     tabline = 1000,
@@ -39,40 +39,84 @@ return {
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'buffers'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'lsp_status'},
-                lualine_z = {'progress', 'location'}
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {
+                    {
+                        'buffers',
+                        show_filename_only = true,   -- Shows shortened relative path when set to false.
+                        hide_filename_extension = false,   -- Hide filename extension when set to true.
+                        show_modified_status = true, -- Shows indicator when the buffer is modified.
+
+                        mode = 2, -- 0: Shows buffer name
+                                    -- 1: Shows buffer index
+                                    -- 2: Shows buffer name + buffer index
+                                    -- 3: Shows buffer number
+                                    -- 4: Shows buffer name + buffer number
+
+                        max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+                                                            -- it can also be a function that returns
+                                                            -- the value of `max_length` dynamically.
+                        filetype_names = {
+                            TelescopePrompt = 'Telescope',
+                            dashboard = 'Dashboard',
+                            packer = 'Packer',
+                            fzf = 'FZF',
+                            alpha = 'Alpha'
+                        }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+                        -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+                        use_mode_colors = false,
+
+                        buffers_color = {
+                            -- Same values as the general color option can be used here.
+                            active = 'lualine_a_normal',     -- Color for active buffer.
+                            inactive = 'lualine_a_inactive', -- Color for inactive buffer.
+                        },
+
+                        symbols = {
+                            modified = ' ●',      -- Text to show when the buffer is modified
+                            alternate_file = '#', -- Text to show to identify the alternate file
+                            directory =  '',     -- Text to show when the buffer is a directory
+                        },
+                    }
+                },
             },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
                 lualine_c = {},
-                lualine_x = {'location'},
+                lualine_x = {},
                 lualine_y = {},
                 lualine_z = {}
             },
-            tabline = {},
+            tabline = {
+                lualine_a = {'tabs'},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {'windows'}
+            },
             winbar = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'buffers'},
-                lualine_x = {},
+                lualine_a = {'location', 'filename'},
+                lualine_b = {'progress'},
+                lualine_c = {},
+                lualine_x = {'encoding', 'fileformat'},
                 lualine_y = {},
-                lualine_z = {}
+                lualine_z = {'lsp_status', 'filetype'}
             },
-
             inactive_winbar = {
-                lualine_a = {},
+                lualine_a = {'location', 'filename'},
                 lualine_b = {},
-                lualine_c = {'buffers'},
+                lualine_c = {},
                 lualine_x = {},
                 lualine_y = {},
-                lualine_z = {}
+                lualine_z = {'lsp_status', 'filetype'}
             },
-            extensions = {}
+            extensions = {'quickfix', 'nvim-tree'}
             }
-
         end,
     },
 }
