54a6b488b7e53a979c51e9804b0827c6b7294a19
Yuxuan Li <rouge_3877@outlook.com>
2025-09-21 13:37:37 +0800

    ✨ Change lualine theme from 'moonfly' to 'horizon' for improved aesthetics

--- cli/nvim/dot/.config/nvim/lua/plugins/appearance/lualine.lua
+++ cli/nvim/dot/.config/nvim/lua/plugins/appearance/lualine.lua
@@ -6,7 +6,7 @@ return {
             require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'moonfly',
+                theme = 'horizon',
                 section_separators = { left = '', right = '' },
                 component_separators = { left = '', right = '' },
                 disabled_filetypes = {
